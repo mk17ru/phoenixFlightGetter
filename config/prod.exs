a08a47b7d2cd553133658882a2988621cb1dc74e
mk17ru <mk17.ru@mail.ru>
2021-06-02 00:30:36 +0500

    Use

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :project, ProjectWeb.Endpoint,
        http: [port: {:system, "PORT"}],
-       url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+       url: [scheme: "https", host: "salty-lake-76452.herokuapp.com", port: 443],
        force_ssl: [rewrite_on: [:x_forwarded_proto]],
        cache_static_manifest: "priv/static/cache_manifest.json"
 
